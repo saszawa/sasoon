d69967c75e6b0f06096e3eaae110846583455e51
shun <shun.ozawa34@gmail.com>
2014-07-13 19:26:00 +0900

    stage登録

--- js/game.js
+++ js/game.js
@@ -3897,9 +3897,24 @@ function makeJSON(stages){
   console.log(json);
 }
 
-function doPost(action){
-  var submitType = document.createElement("input");
-  submitType.setAttribute("type","hidden");
+function doPost(stageString,userName){
+
+  userName = userName || '';
+  var postData = 'user_name='+userName+'&stage_jsarray='+stageString;
+
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.onreadystatechange = function (){
+    if ((xmlHttp.readyState == 4) && (xmlHttp.status == 200)){
+      if(xmlHttp.responseText === 'SUCCESS'){
+
+      } else if(xmlHttp.responseText === 'FAILED'){}
+    }
+  }
+
+  xmlHttp.open("POST", "php/stageRegist.php");
+  xmlHttp.setRequestHeader( 'Content-Type', 'application/x-www-form-urlencoded' );
+  xmlHttp.send(postData);
+
 }
 
 var TestPlayButton = Class.create(ExLabel,{
--- js/stageedit/Post.js
+++ js/stageedit/Post.js
@@ -1,4 +1,21 @@
-function doPost(action){
-  var submitType = document.createElement("input");
-  submitType.setAttribute("type","hidden");
+function doPost(stageString,userName){
+
+  userName = userName || '';
+  var postData = 'user_name='+userName+'&stage_jsarray='+stageString;
+
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.onreadystatechange = function (){
+    if ((xmlHttp.readyState == 4) && (xmlHttp.status == 200)){
+      if(xmlHttp.responseText === 'SUCCESS'){
+
+      } else if(xmlHttp.responseText === 'FAILED'){
+        
+      }
+    }
+  }
+
+  xmlHttp.open("POST", "php/stageRegist.php");
+  xmlHttp.setRequestHeader( 'Content-Type', 'application/x-www-form-urlencoded' );
+  xmlHttp.send(postData);
+
 }
